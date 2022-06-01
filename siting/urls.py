dcf6cf5e4505cab54b652ab207cffd55cf701164
PraneethKarnena <praneeth.codes@gmail.com>
2022-06-01 16:10:05 +0530

    Add home page endpoint

--- siting/urls.py
+++ siting/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from siting.views import AnimalView, BreedView, SitingView
+from siting.views import AnimalView, BreedView, SitingView, HomeView
 
 
 urlpatterns = [
     path('animals/', AnimalView.as_view(), name='animals_list'),
     path('breeds/<uuid:animal_id>/', BreedView.as_view(), name='breeds_list'),
     path('sitings/', SitingView.as_view(), name='sitings_list'),
+    path('', HomeView.as_view(), name='home'),
 ]
