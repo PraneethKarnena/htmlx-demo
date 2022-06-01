3730f6e6da878776badb11951de0bb3ae1d0da9c
PraneethKarnena <praneeth.codes@gmail.com>
2022-06-01 15:00:31 +0530

    Add sitings endpoint

--- siting/urls.py
+++ siting/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from siting.views import AnimalView, BreedView
+from siting.views import AnimalView, BreedView, SitingView
 
 
 urlpatterns = [
     path('animals/', AnimalView.as_view(), name='animals_list'),
     path('breeds/<uuid:animal_id>/', BreedView.as_view(), name='breeds_list'),
+    path('sitings/', SitingView.as_view(), name='sitings_list'),
 ]
