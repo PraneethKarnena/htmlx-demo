c5ac2aa1df6d1d08e3592a1b11e0940ba8e13366
PraneethKarnena <praneeth.codes@gmail.com>
2022-06-01 14:47:38 +0530

    Add breeds endpoint

--- siting/urls.py
+++ siting/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from siting.views import AnimalView
+from siting.views import AnimalView, BreedView
 
 
 urlpatterns = [
     path('animals/', AnimalView.as_view(), name='animals_list'),
+    path('breeds/<uuid:animal_id>/', BreedView.as_view(), name='breeds_list'),
 ]
