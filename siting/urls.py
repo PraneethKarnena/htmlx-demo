d490793788245eca8401c80316e7aa0a94629a0b
PraneethKarnena <praneeth.codes@gmail.com>
2022-06-02 12:49:04 +0530

    Add endpoint for dates

--- siting/urls.py
+++ siting/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from siting.views import AnimalView, BreedView, SitingView, HomeView
+from siting.views import AnimalView, BreedView, SitingView, HomeView, DateView
 
 
 urlpatterns = [
     path('animals/', AnimalView.as_view(), name='animals_list'),
     path('breeds/', BreedView.as_view(), name='breeds_list'),
     path('sitings/', SitingView.as_view(), name='sitings_list'),
+    path('dates/', DateView.as_view(), name='date_list'),
     path('', HomeView.as_view(), name='home'),
 ]
