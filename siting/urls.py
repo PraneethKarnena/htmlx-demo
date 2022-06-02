dafc98cab9ebc2d527b976fd976109b49318c920
PraneethKarnena <praneeth.codes@gmail.com>
2022-06-02 11:30:09 +0530

    Remove animal_id slug on breeds endpoint

--- siting/urls.py
+++ siting/urls.py
@@ -5,7 +5,7 @@ from siting.views import AnimalView, BreedView, SitingView, HomeView
 
 urlpatterns = [
     path('animals/', AnimalView.as_view(), name='animals_list'),
-    path('breeds/<uuid:animal_id>/', BreedView.as_view(), name='breeds_list'),
+    path('breeds/', BreedView.as_view(), name='breeds_list'),
     path('sitings/', SitingView.as_view(), name='sitings_list'),
     path('', HomeView.as_view(), name='home'),
 ]
